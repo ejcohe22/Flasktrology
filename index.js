39351c5c7b012aa515151a5987afceb17062db4e
Erik Cohen <ejcohe22@colby.edu>
2022-11-24 10:40:41 -0600

    clean up some style errors + add some documentation

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Request Variables
-const baseUrl = "http://sandipbgt.com/theastrologer/api";
-const astro = {signs: ['stuff']}
+const baseUrl = 'http://sandipbgt.com/theastrologer/api';
+const astro = {signs: ['stuff']};
 
 app.get('/', async function(req, res) {
-  await get_sunsigns()
-    .then(res => astro['signs'] = res.data)
-    .catch(err => console.log(err));
+  await getSunsigns()
+      .then((res) => astro['signs'] = res.data)
+      .catch((err) => console.log(err));
 
-  //Object.values(sunsigns).forEach((sign)=> console.log(sign));
+  // Object.values(sunsigns).forEach((sign)=> console.log(sign));
 
   res.render( 'index', {sunsigns: astro['signs']} );
 });
@@ -27,7 +27,12 @@ app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
 
-// get sun signs in an array as request
-async function get_sunsigns(){
-  return await axios.get(`${baseUrl}/sunsigns`)
+/**
+ * Gets a list of sunsigns. Call into sandipbgpt api
+ * https://github.com/sandipbgt/theastrologer-api
+ *
+ * @return {promise} promise with a list of sunsigns used by the API
+ */
+async function getSunsigns() {
+  return await axios.get(`${baseUrl}/sunsigns`);
 }
