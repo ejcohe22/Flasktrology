06d8c8a19da93f231025d2198ce15229217cfb87
Erik Cohen <ejcohe22@colby.edu>
2022-11-24 12:55:43 -0600

    working filepath for local

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Request Variables
 const baseUrl = 'http://sandipbgt.com/theastrologer/api';
