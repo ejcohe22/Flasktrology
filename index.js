8ae8353d46c4b6714ae4928be2c2bfae64e5c58e
Erik Cohen <ejcohe22@colby.edu>
2022-11-24 10:31:13 -0600

    restructure for dynamic home page, first axios req

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
+// Server Variables
 const app = express();
 const port = process.env.PORT || 5000;
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+// Request Variables
+const baseUrl = "http://sandipbgt.com/theastrologer/api";
+const astro = {signs: ['stuff']}
+
+app.get('/', async function(req, res) {
+  await get_sunsigns()
+    .then(res => astro['signs'] = res.data)
+    .catch(err => console.log(err));
+
+  //Object.values(sunsigns).forEach((sign)=> console.log(sign));
+
+  res.render( 'index', {sunsigns: astro['signs']} );
 });
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
+
+// get sun signs in an array as request
+async function get_sunsigns(){
+  return await axios.get(`${baseUrl}/sunsigns`)
+}
